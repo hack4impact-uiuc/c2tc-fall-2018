84d28d726b2020c99959ffb227a2a6f01fa350fc
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2018-10-09 22:11:23 -0500

    Restore App class in App.js, remove drawer component, add pin generation to map, restore double tap to zoom functionality

--- c2tc-mobile/App.js
+++ c2tc-mobile/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import { DrawerNavigator } from 'react-navigation';
-import { Dimensions } from 'react-native';
+import React, { Component } from 'react';
 
 import HomeScreen from './screens/LiveLocation';
 //import HomeScreen from './screens/CustomOverlay';
 
-var {height, width} = Dimensions.get('window');
+export default class App extends Component {
 
-export default DrawerNavigator({
-    'Home': {
-        screen: HomeScreen,
-    },
-}, {
-    initialRouteName: 'Home',
-    drawerWidth: width / 2,
-    drawerPosition: 'left',
-    drawerOpenRoute: 'DrawerOpen',
-    drawerCloseRoute: 'DrawerClose',
-    drawerToggleRoute: 'DrawerToggle',
-    drawerBackgroundColor: 'green',
-    contentOptions: {
-        activeTintColor: 'red',
-    },
-    order: ['Home']
-} );
+    render() {
+        return (
+            <HomeScreen></HomeScreen>
+        );
+      }
+}
--- c2tc-mobile/screens/HomeScreen.js
+++ c2tc-mobile/screens/HomeScreen.js
@@ -1,4 +1,3 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, AsyncStorage, TouchableHighlight, Image } from 'react-native';
 import MapView from 'react-native-maps';
-
