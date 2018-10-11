8be6d85e446a89d8e5a377719871ed0ad15da1ce
Daniel Choi <daniel.won.choi@gmail.com>
2018-10-11 16:41:12 -0500

    panel done

--- c2tc-mobile/App.js
+++ c2tc-mobile/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import LiveLocation from './screens/LiveLocation';
+import Panel from './components/Panel'
+
 export default class App extends Component {
 
     render() {
         return (
             <LiveLocation></LiveLocation>
+            //<Panel />
         );
       }
 }
