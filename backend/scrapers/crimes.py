1101e09499cbad0edb441ff0a040f17ac0758daa
Josh Burke <joshburke.school@gmail.com>
2018-10-18 20:42:05 -0500

    Formatted.

--- backend/scrapers/crimes.py
+++ backend/scrapers/crimes.py
@@ -6,27 +6,30 @@ import datetime
 app_token = "3vAi4grxLm8Lql1sffmqGNo2o"
 api_url = "https://moto.data.socrata.com/resource/3h5f-6xbh.json"
 
-days_of_crime = 30 #How many days of crime data to pull from API
+days_of_crime = 30  # How many days of crime data to pull from API
 
-#Columns to select from db
-req_fields = ['incident_id',
-    'incident_datetime',
-    'incident_type_primary',
-    'incident_description',
-    'address_1',
-    'city',
-    'state',
-    'latitude',
-    'longitude',
-    'hour_of_day',
-    'day_of_week',
-    'parent_incident_type']
+# Columns to select from db
+req_fields = [
+    "incident_id",
+    "incident_datetime",
+    "incident_type_primary",
+    "incident_description",
+    "address_1",
+    "city",
+    "state",
+    "latitude",
+    "longitude",
+    "hour_of_day",
+    "day_of_week",
+    "parent_incident_type",
+]
+
 
 def get_qs_url(url, args):
-    '''
+    """
     Accepts url string and dictionary of querystring parameters, returns properly
     formatted url.
-    '''
+    """
     qs_url = url
     i = 0
     for k, v in args.items():
@@ -38,23 +41,25 @@ def get_qs_url(url, args):
         i += 1
     return qs_url
 
+
 def get_datetime(days_ago):
-    '''
+    """
     Returns datetime as formatted floating datetime string.
     E.g.: 2015-01-10T14:00:00.000
-    '''
+    """
     today = datetime.date.today()
     offset = datetime.timedelta(days=days_ago)
     bound = today - offset
     bound_str = str(bound) + "T06:00:00.000"
     return bound_str
 
+
 def pull_data(headers, payload, api_url):
-    '''
+    """
     Pulls data from Socrata API, returns dict of all data. Could probably write a
     generic scraper class and this would take only a little tweaking to work for
     every scraper.
-    '''
+    """
     return_data = {}
     data = requests.get(get_qs_url(api_url, payload), headers=headers)
     for raw_record in data.json():
