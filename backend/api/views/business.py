f8d35c1b4bb3c0703686279588e5353f2a4ae80c
Josh Burke <joshburke.school@gmail.com>
2018-10-25 22:21:04 -0500

    start of open_business endpt

--- backend/api/views/business.py
+++ backend/api/views/business.py
@@ -6,6 +6,24 @@ from api.core import create_response, serialize_list, logger
 
 business = Blueprint("business", __name__)
 
+
+@business.route("/open_businesses", methods=["GET"])
+def open_businesses():
+    '''
+    Querystring args:   time= #### (time as 4 digit 24hr time, eg. 1430 = 2:30pm)
+                        day = # (integer 0-6, where 0 is Monday)
+    '''
+    data = Business.objects()
+    time = int(request.args.get("time"))
+    day = int(request.args.get("day"))
+    open_businesses = []
+    for b in data:
+        if int(b.open_hours[day].start) <= time && b.open_hours[day].end >= time:
+            #open
+            open_businesses.append(b)
+    ret_data = {"businesses": open_businesses}
+    return create_response(data=ret_data, message="Success", status=201)
+
 @business.route("/businesses", methods=["GET"])
 def get_business():
     """
@@ -22,13 +40,13 @@ def create_business():
     POST function for posting a hard-coded Business object for testing purposes
     """
     location = Location(city="Champaign", country="USA", address1="addy1", state="IL", zip_code="12345")
-    
-    open_hours = OpenHours(start="0000", end="1111", is_overnight=True, day=3)    
+
+    open_hours = OpenHours(start="0000", end="1111", is_overnight=True, day=3)
 
     business = Business.objects.create(
-        name="McDonalds", 
-        yelp_id="asdasd", 
-        image_url="asdasd.com", 
+        name="McDonalds",
+        yelp_id="asdasd",
+        image_url="asdasd.com",
         display_phone="12345555"
     )
     business.location = location
