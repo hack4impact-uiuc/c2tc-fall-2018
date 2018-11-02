a09f5b0252c61e1fc9c9bbfa86794b7494e7215f
Daniel Choi <daniel.won.choi@gmail.com>
2018-11-01 21:39:13 -0500

    format

--- c2tc-mobile/screens/LiveLocation.js
+++ c2tc-mobile/screens/LiveLocation.js
@@ -5,8 +5,7 @@ import MapView, { Marker, ProviderPropType } from "react-native-maps";
 
 import Panel from "../components/PanelComponent/Panel";
 import PhoneButton from "../components/PhoneButton";
-import API from "../components/API"
-
+import API from "../components/API";
 
 const { width, height } = Dimensions.get("window");
 const ASPECT_RATIO = width / height;
