884243ade2ab5d64254d4d121d35d0ad9880563a
anoojlal <anoojl2@illinois.edu>
2018-11-13 21:07:59 -0600

    boutta switch branches boi

--- backend/tests/test_business.py
+++ backend/tests/test_business.py
@@ -5,7 +5,7 @@ import mongomock
 # client passed from client - look into pytest for more info about fixtures
 # test client api: http://flask.pocoo.org/docs/1.0/api/#test-client
 def test_index(client):
-    rs = client.get("/")
+    rs = client.get("/businesses")
     assert rs.status_code == 200
 
 
