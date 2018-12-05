f930ab0ecade1c2e01ab30530a038469ccb43004
Josh Burke <joshburke.school@gmail.com>
2018-12-04 21:46:30 -0600

    testing thing

--- backend/api/views/business.py
+++ backend/api/views/business.py
@@ -34,8 +34,10 @@ def open_businesses():
         if int(curr_day.start) <= time and int(curr_day.end) >= time:
             # open
             open_businesses.append(b.to_mongo())
-        elif prev_day != None and prev_day.is_overnight == "true" and int(prev_day.end) >= time:
-            open_businesses.append(b.to_mongo())
+        if prev_day != None:
+            print(type(prev_day.is_overnight))
+        #elif prev_day != None and prev_day.is_overnight == "true" and int(prev_day.end) >= time:
+            #open_businesses.append(b.to_mongo())
     ret_data = {"businesses": open_businesses}
     return create_response(data=ret_data, message="Success", status=201)
 
