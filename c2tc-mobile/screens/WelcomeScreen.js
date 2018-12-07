e161a10f8e9c90750277eeb9311571881c891490
pkgamma <pkgamma@gmail.com>
2018-12-06 22:08:38 -0600

    basic elements are there, animation is working but needs to get sequence working.

--- c2tc-mobile/screens/WelcomeScreen.js
+++ c2tc-mobile/screens/WelcomeScreen.js
@@ -1,10 +1,133 @@
 import React from "react";
-import { Text } from "react-native";
+import { Animated, View, Image } from "react-native";
+
+class FadeInView extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidMount() {
+    Animated.timing(                  // Animate over time
+      this.state.fadeAnim,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: 1500,              // Make it take a while
+      }
+    ).start();                        // Starts the animation
+  }
+
+  render() {
+    let { fadeAnim } = this.state;
+
+    return (
+      <Animated.View                 // Special animatable View
+        style={{
+          ...this.props.style,
+          opacity: fadeAnim,         // Bind opacity to animated value
+        }}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
 
 class WelcomeScreen extends React.Component {
   render() {
     return (
-        <Text>Hello World</Text>
+      <View>
+
+
+        <FadeInView>
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 230,
+            height: 90,
+            marginTop: 70,
+          }}
+          source={require('../assets/images/welcome/1.png')}
+          resizeMode="contain"
+        />
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 330,
+            height: 160,
+            marginTop: -15,
+          }}
+          source={require('../assets/images/welcome/2.png')}
+          resizeMode="contain"
+        />
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 240,
+            height: 120,
+            marginTop: -25,
+          }}
+          source={require('../assets/images/welcome/3.png')}
+          resizeMode="contain"
+        />
+        <View style={{flexDirection: 'row', alignSelf: 'center',
+            marginTop: -40,}}>
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 30,
+            height: 30,
+            margin: 10,
+          }}
+          source={require('../assets/images/welcome/4-1.png')}
+          resizeMode="contain"
+        />
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 30,
+            height: 30,
+            margin: 10,
+          }}
+          source={require('../assets/images/welcome/4-2.png')}
+          resizeMode="contain"
+        />
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 30,
+            height: 30,
+            margin: 10,
+          }}
+          source={require('../assets/images/welcome/4-3.png')}
+          resizeMode="contain"
+        />
+        </View>
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 350,
+            height: 350,
+            margin: 10,
+          }}
+          source={require('../assets/images/welcome/5.png')}
+          resizeMode="contain"
+        />
+        </FadeInView>
+        
+
+        <Image
+          style={{
+            alignSelf: 'center',
+            width: 500,
+            height: 500,
+            margin: -355,
+          }}
+          source={require('../assets/images/welcome/6.png')}
+          resizeMode="contain"
+        />
+
+      </View>
     );
   }
 }
