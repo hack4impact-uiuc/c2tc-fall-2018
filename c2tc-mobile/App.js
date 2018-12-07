bf9f3d1e263860d3add1e037d53f29f776388af9
pkgamma <pkgamma@gmail.com>
2018-12-06 20:45:59 -0600

    created WelcomeScreen

--- c2tc-mobile/App.js
+++ c2tc-mobile/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import LiveLocation from "./screens/LiveLocation";
+import WelcomeScreen from "./screens/WelcomeScreen"
 
 export default class App extends Component {
   render() {
-    return <LiveLocation />;
+    return <WelcomeScreen />;
   }
 }
---
+++ c2tc-mobile/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+class WelcomeScreen extends React.Component {
+  render() {
+    return (
+        <Text>Hello World</Text>
+    );
+  }
+}
+
+export default WelcomeScreen;
