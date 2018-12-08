fbcb63f1a70fef697a3c19dba2754d48c66506d0
pkgamma <pkgamma@gmail.com>
2018-12-08 16:24:06 -0600

    done, need to add continue and back button, then MANUALLY merge to master

--- c2tc-mobile/App.js
+++ c2tc-mobile/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import LiveLocation from "./screens/LiveLocation";
-import WelcomeScreen from "./screens/WelcomeScreen"
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default class App extends Component {
   render() {
