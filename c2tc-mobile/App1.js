d8266ae128ea9680a60c7b5b6c86856c2ac15e7d
Megha Mallya <meghamallya1@gmail.com>
2019-03-07 09:23:47 -0600

    redux is added to everything but livelocation

--- c2tc-mobile/App1.js
+++ c2tc-mobile/App1.js
@@ -2,51 +2,38 @@ import React, { Component } from "react";
 import { StyleSheet, AsyncStorage } from "react-native";
 import { createStackNavigator } from "react-navigation";
 
-import MapScreen from "./screens/MapScreen";
+import LiveLocation from "./screens/LiveLocation";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import IntroScreen from "./screens/IntroScreen";
-import { Provider, connect } from 'react-redux';
-import {store, reduxifiedNavigator} from "./Redux"
 
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      isLoaded: false
-    };
   }
   async componentDidMount() {
     if (AsyncStorage.getAllKeys().length != 1) {
       await AsyncStorage.setItem("loaded", JSON.stringify(1));
     } else {
-      this.setState({
-        isLoaded: true
-      });
+      this._mounted = true;
     }
   }
 
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+
   render() {
-    return (
-      <Navigator/>
-    )
-    // if (this.state.isLoaded) {
-    //   return(
-    //     <Provider store={store}>
-    //       <LiveLocation  navigation={this.props.navigation} />
-    //     </Provider> 
-    //   );
-    // }
+    if (this._mounted) {
+      return <LiveLocation navigation={this.props.navigation} />;
+    }
 
-    // return <IntroScreen navigation={this.props.navigation} />
-
-
+    return <IntroScreen navigation={this.props.navigation} />;
   }
 }
 
-Navigator = createStackNavigator({
+export default createStackNavigator({
   Intro: {
-    screen: IntroScreen,
+    screen: App,
     navigationOptions: {
       header: null,
       headerMode: "screen"
@@ -60,7 +47,7 @@ Navigator = createStackNavigator({
     }
   },
   MapScreen: {
-    screen: MapScreen,
+    screen: LiveLocation,
     navigationOptions: {
       header: null,
       headerMode: "screen"
---
+++ c2tc-mobile/components/NavigationComponents/ButtonInterface.1.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from "react-native";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import {updateRenderData,updateMarkers } from '../../Redux'
+import renderLayerMarkers from '../MapRendering'
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      updateMarkers,
+      updateRenderData
+    },
+    dispatch
+  )
+}
+
+
+const mapStateToProps = state => {
+  return { 
+    renderData:state.renderData,
+    layerData: state.layerData,
+    colorData: state.colorData,
+    markers: state.markers,
+    mapRegion: state.mapRegion
+  };
+};
+
+class ButtonInterface extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _onPressToggleLayers = layer => {
+    if (this.props.renderData[layer]) {
+      this.props.updateRenderData(layer,false)
+      let markers = this.props.markers.filter(
+        marker => marker["color"] !== this.props.colorData[layer]
+      )
+      this.props.updateMarkers(
+        markers
+      ) 
+        
+    } else {
+      this.renderMarkers(
+        layer,
+        this.props.layerData[layer],
+        this.props.colorData[layer]
+      );
+      this.props.updateRenderData(layer,true)
+    }
+  };
+
+  async renderMarkers(layer, data, markerColor) {
+    const { layerData,colorData, markers,mapRegion } = this.props
+    let markerList = await renderLayerMarkers(layer,data,markerColor,layerData,colorData,markers,mapRegion)
+    this.props.updateMarkers(markerList)
+  }
+
+  updateLayer = () => {
+    this._onPressToggleLayers(this.props.type)
+  };
+
+  render() {
+    var isSelected = this.props.renderData[this.props.type]
+    return (
+      <View style={styles.view}>
+        <TouchableOpacity
+          onPress={this._onPressToggleLayers(this.props.type)}
+          style={
+            isSelected
+              ? [styles.selectedButton, { backgroundColor: this.props.color }]
+              : styles.unselectedButton
+          }
+        >
+          <FontAwesome
+            name={this.props.icon}
+            size={32}
+            color={isSelected ? "white" : this.props.color}
+          />
+          <Text
+            style={isSelected ? styles.selectedText : styles.unselectedText}
+          >
+            {this.props.name}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ButtonInterface);
+
+const styles = StyleSheet.create({
+  selectedButton: {
+    borderRadius: 9,
+    alignContent: "center",
+    flexDirection: "row",
+    borderWidth: 1.5,
+    borderColor: "rgba(142,142,147,0)",
+    flexWrap: "wrap",
+    padding: 10
+  },
+  unselectedButton: {
+    flexDirection: "row",
+    alignContent: "center",
+    flexWrap: "wrap",
+    borderColor: "rgba(142,142,147,0.70)",
+    borderWidth: 1.5,
+    backgroundColor: "white",
+    borderRadius: 9,
+    padding: 10
+  },
+  view: {
+    width: Dimensions.get("window").width / 2 - 10,
+    height: 60,
+    padding: 3
+  },
+  icon: {
+    position: "relative",
+    width: 35
+  },
+  selectedText: {
+    paddingLeft: 10,
+    textAlign: "left",
+    fontWeight: "400",
+    width: Dimensions.get("window").width / 2 - 75,
+    flexWrap: "wrap",
+    height: 50,
+    fontSize: 18,
+    color: "white"
+  },
+  unselectedText: {
+    paddingLeft: 10,
+    textAlign: "left",
+    fontWeight: "400",
+    flexWrap: "wrap",
+    width: Dimensions.get("window").width / 2 - 75,
+    height: 50,
+    fontSize: 18,
+    color: "#8e8e93"
+  }
+});
