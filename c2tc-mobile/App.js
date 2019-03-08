f9ecf62d4406f56669e2567a7d6343fb5c9e043c
Alice Fang <alicesf2@illinois.edu>
2019-03-07 19:21:01 -0600

    added api calls

--- c2tc-mobile/App.js
+++ c2tc-mobile/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, AsyncStorage } from "react-native";
 import { createStackNavigator } from "react-navigation";
+import api from "./components";
 
 import LiveLocation from "./screens/LiveLocation";
 import WelcomeScreen from "./screens/WelcomeScreen";
@@ -11,6 +12,14 @@ class App extends Component {
     super(props);
   }
   async componentDidMount() {
+    console.log(api.getUsers());
+    data = {
+      net_id: "alicesf2",
+      username: "alicesf2",
+      verified: False,
+      anon: False
+    };
+    console.log(api.createUser(data));
     if (AsyncStorage.getAllKeys().length != 1) {
       await AsyncStorage.setItem("loaded", JSON.stringify(1));
     } else {
