78273baf7c0fd028924b0c5282f77024f13b4af9
Megha Mallya <meghamallya1@gmail.com>
2019-03-10 15:14:41 -0500

    added api into code

--- c2tc-mobile/App.js
+++ c2tc-mobile/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { StyleSheet, AsyncStorage } from "react-native";
 import { createStackNavigator } from "react-navigation";
+import API from "./components/API";
 
 import MapScreen from "./screens/MapScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import IntroScreen from "./screens/IntroScreen";
 import TipForm from "./screens/TipForm";
-import { Provider, connect } from "react-redux";
-import { store, reduxifiedNavigator } from "./Redux";
 import TipOverviewScreen from "./screens/TipOverviewScreen";
 import TipDetailsScreen from "./screens/TipDetailsScreen";
 
@@ -67,14 +66,12 @@ Navigator = createStackNavigator({
       header: null,
       headerMode: "screen"
     }
-  }
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  },
+  TipForm: {
+    screen: TipForm,
+    navigationOptions: {
+      header: null,
+      headerMode: "screen"
+    }
   }
 });
