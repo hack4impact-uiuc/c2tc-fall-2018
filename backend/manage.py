8380c268e8caced4cf429f112f5adfb3f9bb51c5
Albert Cao <albertc4@illinois.edu>
2019-03-14 19:39:31 -0500

    deleted empty test files

--- backend/manage.py
+++ backend/manage.py
@@ -13,10 +13,17 @@ app = create_app()
 manager = Manager(app)
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(scrape, 'interval', weeks=4, timezone='America/Indiana/Indianapolis')
-scheduler.add_job(crime_scrape, 'interval', weeks=4, timezone='America/Indiana/Indianapolis')
-scheduler.add_job(business_scrape, 'interval', weeks=4, timezone='America/Indiana/Indianapolis')
-scheduler.add_job(streetlight_scrape, 'interval', weeks=4, timezone='America/Indiana/Indianapolis')
+scheduler.add_job(scrape, "interval", weeks=4, timezone="America/Indiana/Indianapolis")
+scheduler.add_job(
+    crime_scrape, "interval", weeks=4, timezone="America/Indiana/Indianapolis"
+)
+scheduler.add_job(
+    business_scrape, "interval", weeks=4, timezone="America/Indiana/Indianapolis"
+)
+scheduler.add_job(
+    streetlight_scrape, "interval", weeks=4, timezone="America/Indiana/Indianapolis"
+)
+
 
 @app.route("/schedule", methods=["GET"])
 def get_schedule():
@@ -24,9 +31,12 @@ def get_schedule():
     GET function for retrieving all schedules for the scrapers
     """
     jobs = [job.name for job in scheduler.get_jobs()]
-    next_time = [job.next_run_time.strftime("%m/%d/%Y, %H:%M:%S") for job in scheduler.get_jobs()]
+    next_time = [
+        job.next_run_time.strftime("%m/%d/%Y, %H:%M:%S") for job in scheduler.get_jobs()
+    ]
     response = dict(zip(jobs, next_time))
     return create_response(data=response)
+
 
 @manager.command
 def runserver():
