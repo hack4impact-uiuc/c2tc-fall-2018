e391ac96b1a1a00ccb81e179345df2eb7b19a7cb
Albert Cao <albertc4@illinois.edu>
2019-04-04 20:35:01 -0500

    reformatted

--- backend/api/views/tips.py
+++ backend/api/views/tips.py
@@ -30,16 +30,25 @@ def get_tip(id):
     response = Tips.objects.get(id=id).to_mongo()
     return create_response(data=dict(response))
 
+
 @tips.route("/tips_near", methods=["GET"])
 def get_tips_near():
-     """
+    """
      GET function for retrieving all tips within a certain radius of a longitude and latitude
      """
-     latitude = request.args.get('lat')
-     longitude = request.args.get('long')
-     response = [tips.to_mongo() for tips in Tips.objects if distance.distance((tips.latitude, tips.longitude), (latitude, longitude)).miles <= 0.1]
-     response = {"tips": response}
-     return create_response(data=response)
+    latitude = request.args.get("lat")
+    longitude = request.args.get("long")
+    response = [
+        tips.to_mongo()
+        for tips in Tips.objects
+        if distance.distance(
+            (tips.latitude, tips.longitude), (latitude, longitude)
+        ).miles
+        <= 0.1
+    ]
+    response = {"tips": response}
+    return create_response(data=response)
+
 
 @tips.route("/user/<user_id>/tips", methods=["GET"])
 def get_tips_by_user(user_id):
@@ -100,6 +109,7 @@ def get_verified_tips():
     response = [tip.to_mongo() for tip in Tips.objects if tip.verified == True]
     response = {"verified_tips": response}
     return create_response(data=response)
+
 
 @tips.route("/tips/pending", methods=["GET"])
 def get_pending_tips():
