e2c3fa43316460aaf9e13493d25918f6f4ab5216
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-04-09 21:27:41 -0500

    Format and add handle login

--- c2tc-mobile/screens/LoginScreen.js
+++ c2tc-mobile/screens/LoginScreen.js
@@ -6,7 +6,7 @@ import {
   View,
   TouchableOpacity,
   Text,
-  ScrollView,
+  ScrollView
 } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { TextInput } from "react-native-paper";
@@ -21,56 +21,54 @@ export default class Login extends Component {
     pswd: ""
   };
 
+  handleLogin = async () => {};
+
   render() {
     return (
-        <KeyboardAvoidingView
-          style={styles.wrapper}
-          behavior="padding"
-          keyboardVerticalOffset={0}
-        >
-          <View style={styles.backHeader}>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate("TipOverview")}
-              style={styles.backButton}
-            >
-              <Text style={styles.backText}>
-                <FontAwesome name="chevron-left" size={20} color="#027BFF" />{" "}
-                Back
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <ScrollView
-            style={styles.container}
-            keyboardShouldPersistTaps={"always"}
-            removeClippedSubviews={false}
+      <KeyboardAvoidingView
+        style={styles.wrapper}
+        behavior="padding"
+        keyboardVerticalOffset={0}
+      >
+        <View style={styles.backHeader}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("TipOverview")}
+            style={styles.backButton}
           >
-        <Text style={styles.full_header}>Login</Text>
-            <Text style={styles.header}>Username</Text>
-            <TextInput
-              mode="outlined"
-              style={styles.inputContainerStyle}
-              label="Username"
-              placeholder="Username"
-              value={this.state.usr}
-              onChangeText={username => this.setState({ usr })}
-            />
-            <Text style={styles.header}>Password</Text>
-            <TextInput
-              mode="outlined"
-              style={styles.inputContainerStyle}
-              label="Password"
-              placeholder="Password"
-              value={this.state.pswd}
-              onChangeText={pswd => this.setState({ pswd })}
-            />
-            <TouchableOpacity
-              style={styles.login_btn}
-              // onPress={this.handSubmitTip}
-            >
-              <Text style={styles.button_text}>Login</Text>
-            </TouchableOpacity>
-          </ScrollView>
-        </KeyboardAvoidingView>
+            <Text style={styles.backText}>
+              <FontAwesome name="chevron-left" size={20} color="#027BFF" /> Back
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView
+          style={styles.container}
+          keyboardShouldPersistTaps={"always"}
+          removeClippedSubviews={false}
+        >
+          <Text style={styles.full_header}>Login</Text>
+          <Text style={styles.header}>Username</Text>
+          <TextInput
+            mode="outlined"
+            style={styles.inputContainerStyle}
+            label="Username"
+            placeholder="Username"
+            value={this.state.usr}
+            onChangeText={username => this.setState({ usr })}
+          />
+          <Text style={styles.header}>Password</Text>
+          <TextInput
+            mode="outlined"
+            style={styles.inputContainerStyle}
+            label="Password"
+            placeholder="Password"
+            value={this.state.pswd}
+            onChangeText={pswd => this.setState({ pswd })}
+          />
+          <TouchableOpacity style={styles.login_btn} onPress={this.handleLogin}>
+            <Text style={styles.button_text}>Login</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </KeyboardAvoidingView>
     );
   }
 }
