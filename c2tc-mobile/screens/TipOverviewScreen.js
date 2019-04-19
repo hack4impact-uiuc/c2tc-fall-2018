4bb04e257bfa822e117035bb85fce7742be190d9
shreyshrey1 <talk2shreyas@gmail.com>
2019-04-18 20:17:35 -0500

    greeting based on time

--- c2tc-mobile/screens/TipOverviewScreen.js
+++ c2tc-mobile/screens/TipOverviewScreen.js
@@ -31,11 +31,13 @@ class TipOverviewScreen extends React.Component {
       user: "Philip",
       currentdate: "Thursday Feb 28",
       screenType: "view",
-      tips: []
+      tips: [], 
+      greeting: ""
     };
   }
 
   async componentDidMount() {
+    this.get_greeting();
     if (this.state.screenType === "view") {
       let tipsResponse = await API.getVerifiedTips();
       this.setState({ tips: tipsResponse });
@@ -83,6 +85,26 @@ class TipOverviewScreen extends React.Component {
     this.props.navigation.navigate("Profile");
   };
 
+  get_greeting = () => {
+    let curr_greeting = "";
+    let d = new Date();
+    let hour = d.getUTCHours();
+    if (hour <= 4) {
+      curr_greeting = "Good Night";
+    } else if (hour > 4 && hour <= 12) {
+      curr_greeting = "Good Morning";
+    } else if (hour > 12 && hour <= 15) {
+      curr_greeting = "Good Afternoon";
+    } else if (hour > 15 && hour <= 19) {
+      curr_greeting = "Good Evening";
+    } else {
+      curr_greeting = "Good Night";
+    }
+    this.setState({
+      greeting: curr_greeting
+    })
+  }
+
   render() {
     const screenStyle = this.state.screenType;
     if (this.props.page !== "tips") {
@@ -106,8 +128,7 @@ class TipOverviewScreen extends React.Component {
                   }
                 ]}
               >
-                Good Evening,{"\n"}
-                {this.state.user}
+                {this.state.greeting + "\n" + this.state.user}
               </Text>
               <TouchableOpacity onPress={this.profilePicPressed}>
                 <Image
