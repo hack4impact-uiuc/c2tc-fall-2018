7a697da7103dc370e6402b480e6cb66fc872ded6
Michael Leon <mal7@illinois.edu>
2019-04-22 18:20:14 -0500

    Login/register helper

--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -6,7 +6,6 @@ auth = Blueprint("auth", __name__)
 
 auth_server_host = "http://localhost:8000/"
 
-
 @auth.route("/register", methods=["POST"])
 def register():
     return auth_helper("register", "email", "password", "role")
@@ -21,16 +20,16 @@ def auth_helper(endpoint, *properties):
     properties = list(properties)
     user_input = request.get_json()
 
-    auth_req_data = {}
+    auth_post_data = {}
     for prop in properties:
         if prop not in user_input:
             return create_response(
                 message="Missing required info!", status=422, data={"status": "fail"}
             )
-        auth_req_data[prop] = user_input[prop]
+        auth_post_data[prop] = user_input[prop]
 
     auth_server_response = requests.post(
-        auth_server_host + endpoint, json=auth_req_data
+        auth_server_host + endpoint, json=auth_post_data
     )
     response_body = auth_server_response.json()
 
