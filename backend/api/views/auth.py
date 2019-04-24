e2d3e6fd1e83e4e8da985b770183370d332c3f3b
Michael Leon <mal7@illinois.edu>
2019-04-23 21:28:47 -0500

    Fixed formatting

--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -8,8 +8,10 @@ from datetime import datetime
 auth = Blueprint("auth", __name__)
 auth_server_host = "http://localhost:8000/"
 
+
 def invalid_email(email_address):
     return not email_address.endswith("@illinois.edu")
+
 
 @auth.route("/register", methods=["POST"])
 @necessary_post_params("email", "password", "net_id", "anon", "username", "role")
@@ -17,7 +19,11 @@ def register():
     client_data = request.get_json()
 
     if invalid_email(client_data["email"]):
-        return create_response(message="Not a valid email to register with!", status=422, data={"status": "fail"})
+        return create_response(
+            message="Not a valid email to register with!",
+            status=422,
+            data={"status": "fail"},
+        )
 
     our_response, code = post_to_auth_server("register", "email", "password", "role")
     if code == 200:
@@ -26,15 +32,17 @@ def register():
         create_new_db_user(request.get_json(), auth_uid)
     return (our_response, code)
 
+
 @auth.route("/login", methods=["POST"])
 @necessary_post_params("email", "password")
 def login():
     return post_to_auth_server("login", "email", "password")
 
+
 def post_to_auth_server(endpoint, *properties_to_post):
     user_input = request.get_json()
 
-    auth_post_data = { key: user_input[key] for key in properties_to_post }
+    auth_post_data = {key: user_input[key] for key in properties_to_post}
 
     auth_server_response = requests.post(
         auth_server_host + endpoint, json=auth_post_data
@@ -47,7 +55,7 @@ def post_to_auth_server(endpoint, *properties_to_post):
         )
     else:
         jwt_token = response_body["token"]
-        our_response_body = { "token": jwt_token, "auth_uid": response_body["uid"] }
+        our_response_body = {"token": jwt_token, "auth_uid": response_body["uid"]}
         our_response, code = create_response(
             message=response_body["message"],
             status=auth_server_response.status_code,
@@ -56,23 +64,28 @@ def post_to_auth_server(endpoint, *properties_to_post):
         our_response.set_cookie("jwt", jwt_token)
         return (our_response, code)
 
+
 @auth.route("/verifyEmail", methods=["POST"])
 @necessary_post_params("pin")
 def verifyEmail():
     token = request.cookies.get("jwt")
-    post_body = { "pin": request.get_json()["pin"] }
+    post_body = {"pin": request.get_json()["pin"]}
     auth_server_res = requests.post(
         auth_server_host + "verifyEmail/",
-        headers = { "Content-Type": "application/json", "token": token, "google": "undefined" },
-        json=post_body
+        headers={
+            "Content-Type": "application/json",
+            "token": token,
+            "google": "undefined",
+        },
+        json=post_body,
     )
 
     response_body = auth_server_res.json()
 
     return create_response(
-        message=response_body["message"],
-        status=auth_server_res.status_code,
+        message=response_body["message"], status=auth_server_res.status_code
     )
+
 
 def create_new_db_user(client_data, auth_uid):
     user = User.objects.create(
@@ -83,6 +96,6 @@ def create_new_db_user(client_data, auth_uid):
         karma=0,
         posted_tips=[],
         date_created=datetime.now(),
-        auth_server_uid=auth_uid
+        auth_server_uid=auth_uid,
     )
     user.save()
